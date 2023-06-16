65a1c717667df73b571e0573c40ad791d2fe13f9
Jean Connelly <jconnelly@cfa.harvard.edu>
2023-06-16 12:51:59 -0400

    Remove Ska_helpers docstring

--- vv_trend/__init__.py
+++ vv_trend/__init__.py
@@ -1,7 +1,4 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-"""
-Ska_helpers is a collection of utilities for the Ska3 runtime environment.
-"""
 from ska_helpers.version import get_version
 
 __version__ = get_version(__package__)
